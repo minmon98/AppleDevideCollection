9dde2fed9c7c026a72be2ea026763cb7be199d1b
Minh Mon <phivanminh10@gmail.com>
2018-07-16 13:06:45 +0700

    setup simple user interface

---
+++ AppleCollection/AppleCollection/CollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionViewCell.swift
+//  AppleCollection
+//
+//  Created by Minh Mon on 7/16/18.
+//  Copyright © 2018 Minh Mon. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    
+    
+}
