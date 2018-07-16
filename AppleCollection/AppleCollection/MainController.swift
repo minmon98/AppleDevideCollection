0edcadb589a25eb001a87af194dd7d8d17ca4f26
Minh Mon <phivanminh10@gmail.com>
2018-07-16 11:44:25 +0700

    create project and initial setup

---
+++ AppleCollection/AppleCollection/MainController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  AppleCollection
+//
+//  Created by Minh Mon on 7/16/18.
+//  Copyright © 2018 Minh Mon. All rights reserved.
+//
+
+import UIKit
+
+class MainController: UIViewController {
+
+    
+    // Main function 
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
